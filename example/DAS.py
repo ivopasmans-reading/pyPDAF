979b075cc43d4b1d8ebff45f49446499c74ad310
Ivo Pasmans <i.c.pasmans@reading.ac.uk>
2024-06-12 14:53:08 +0100

    Refactored DAS to take components.

--- example/DAS.py
+++ example/DAS.py
@@ -17,96 +17,177 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import pyPDAF.PDAF as PDAF
-import PDAF_caller
-
+import numpy as np
+from options import ScreenOutput  
 
 class DAS:
-
-    """Data assimilation system
+    """
+    This class builds the DA system by composition of objects 
+    listed below and contains a method to run the DA system
+    forward in time. 
+    
+    Methods
+    -------
+    forward : 
+        Run DA system forward to next time window.    
 
     Attributes
     ----------
-    assim_dim : `AssimilationDimensions.AssimilationDimensions`
-        an object of AssimilationDimensions
-    filter_options : `FilterOptions.FilterOptions`
-        filtering options
-    infl : `Inflation.Inflation`
-        inflation object
-    localization : `Localization.Localization`
-        localization object
-    model : `Model.Model`
-        model object
-    obs : `OBS.OBS`
-        observation object
     pe : `parallelization.parallelization`
         parallelization object
-    screen : int
-        verbosity of PDAF screen output
+    model : `Model.Model`
+        model object
+    observations : list of `observations.OBS` objects
+        observation object
+    analyzer : `analyzer.Analyzer` object 
+            Objects containing information about DA method. 
+    is_initialized : bool 
+        flag indicating whether DA system is ready for forward run
+    dim_ens : int 
+            Number of ensemble members.
+        
     """
 
-    def __init__(self, pe, model, obs, screen):
-        """constructor
+    def __init__(self, process_control, models, observations, 
+                 analyzer):
+        """
+        Construct the DA system.. 
 
         Parameters
         ----------
-        pe : `parallelization.parallelization`
-            parallelization object
-        model : `Model.Model`
-            model object
-        obs : `OBS.OBS`
-            observation object
-        screen : int
-            verbosity of PDAF screen output
+        process_control : `parallelization.Parallelization` object
+            Object controlling the use of multiple processes.
+        models : list of `model.Model` objects
+            Objects co
+        observations : list of `observations.Obs` objects
+            Objects containing observations and sample operators. 
+        analyzer : `analyzer.Analyzer` object 
+            Objects containing information about DA method. 
+            
         """
-        self.pe = pe
-        self.model = model
-        self.obs = obs
-        self.screen = screen
+        self.pe = process_control
+        self.models = models
+        self.obs = observations 
+        self.analyzer = analyzer
+        self.is_initialized = False 
+        self.dim_ens = len(self.models)
 
-    def init(self, assim_dim, filter_options, infl, localization):
-        """initialise DA system
-
+    def init(self, verbose=ScreenOutput.quiet):
+        """
+        Setup Fortran backend and model for assimilation. 
+        
         Parameters
         ----------
-        assim_dim : `AssimilationDimensions.AssimilationDimensions`
-            an object of AssimilationDimensions
-        filter_options : `FilterOptions.FilterOptions`
-            filtering options
-        infl : `Inflation.Inflation`
-            inflation object
-        localization : `Localization.Localization`
-            a localization info obejct
+        verbose : options.ScreenOuput object 
+            Option for screen output. 
         """
-        # init model
-        self.model.init_field('inputs_online/true_initial.txt',
-                              self.pe.mype_model)
+        # init models
+        for model in self.models:
+            model.init_fields()
 
         # init observations
         PDAF.omi_init(len(self.obs))
-
-        # Initialize PDAF
-        self.assim_dim = assim_dim
-        self.filter_options = filter_options
-        self.infl = infl
-        self.localization = localization
-        PDAF_caller.init_pdaf(self.assim_dim, self.infl,
-                              self.filter_options,
-                              self.localization,
-                              self.model, self.pe,
-                              self.obs, self.screen)
-
-    def forward(self, step, USE_PDAF):
-        """time forward DA system
+        
+        # init PDAF 
+        status = PDAF.init(self.analyzer.filtertype,
+                           self.analyzer.subtype,
+                           0,
+                           self.analyzer.param_i,
+                           self.analyzer.param_r,
+                           self.pe.COMM_model.py2f(),
+                           self.pe.COMM_filter.py2f(),
+                           self.pe.COMM_couple.py2f(), self.assimilatepe.task_id,
+                           self.pe.n_modeltasks, self.pe.filterpe,
+                           self.u_init_ens_pdaf, int(self.verbose),
+                           )
+        
+        # Mark initialization as complete. 
+        self.is_initialized = True
+        
+    def forward(self, step, verbose=ScreenOutput.quiet):
+        """
+        Step the DA system forward in time to 
+        the next assimilation time. 
 
         Parameters
         ----------
         step : int
-            current time step
-        USE_PDAF : bool
-            whether PDAF is used
+            Current time step.
+        verbose : options.ScreenOuput object 
+            Option for screen output. 
+            
         """
-        self.model.step(self.pe, step, USE_PDAF)
-        if USE_PDAF:
-            PDAF_caller.assimilate_pdaf(self.model, self.obs, self.pe,
-                                            self.assim_dim, self.localization,
-                                            self.filter_options.filtertype)
+        # Check if the Fortran backend and model are ready. 
+        if not self.is_initialized:
+            self.init()
+        #Execute predicition step of DA.
+        self.model.step(step)
+        #Execute the update step. 
+        self.analyzer.assimilate(verbose)
+        
+    def u_unit_ens_pdaf(self, filtertype, dim_p, dim_ens, state_p, uinv, ens_p, status_pdaf):
+        """
+        Allocate memory for ensemble members and initialize the different members. 
+
+        Parameters
+        ----------
+        filtertype : int
+            Type of filter.
+        dim_p : int 
+            Size of state vector on this process.
+        dim_ens : int 
+            Number of ensemble members. 
+        state_p : ndarray of shape (dim_p,)
+            1D state vector on local process.
+        uinv : ndarray of shape (dim_ens-1,dim_ens-1)
+            Inverse of U with U by SVD covariance P=VUVt
+        ens_p : ndarray of shape (dim_p,dim_ens)
+            Ensemble state vector on local process. 
+        status_pdaf : int
+            Status of PDAF.
+
+        Returns
+        -------
+        uinv : ndarray of shape (dim_ens-1,dim_ens-1)
+            Inverse of U with U by SVD covariance P=VUVt
+        ens_p : ndarray of shape (dim_p,dim_ens)
+            Ensemble state vector on local process. 
+        status_pdaf : int
+            Status of PDAF.
+            
+        """
+        #Get initial conditions for each ensemble member.
+        ens_p = np.empty(shape=(dim_p, dim_ens), order='F')
+        for n,model in enumerate(self.models):
+            ens_p[:,n] = model.collect_state_pdaf(dim_p, ens_p[:,n])
+        
+        #Get SVD of covariance matrix. Only relevant for SEEK. 
+        uinv = self.analyzer.get_uinv(dim_ens)
+        
+        return state_p, uinv, ens_p, status_pdaf
+        
+    # def assimilate(self):
+    #     """ 
+    #     Update model fields using the DA correction. 
+    #     """
+        
+        
+    #     # Call the Fortran backend to carry out the assimilation.  
+    #     status = PDAF.omi_assimilate_global(self.model.collect_state_pdaf,
+    #                                         self.model.distribute_state_pdaf,
+    #                                         U_init_dim_obs_PDAFomi,
+    #                                         U_obs_op_PDAFomi,
+    #                                         U_prepoststep_ens_pdaf,
+    #                                         U_next_observation_pdaf)
+        
+    #     # Have process control deal with the errors. 
+    #     if status!=0:
+    #         msg = "PDAF.omi_assimilate_global failed."
+    #         self.pe.raise(msg)
+        
+        
+        
+    #     if USE_PDAF:
+    #         PDAF_caller.assimilate_pdaf(self.model, self.obs, self.pe,
+    #                                     self.assim_dim, self.localization,
+    #                                     self.filter_options.filtertype)
